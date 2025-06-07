ce39acdf9c79046bd825461f0b051c424c0d3b2c
3x-haust <lyusungyun@icloud.com>
2025-06-08 03:31:32 +0900

    refactor: 기존 파일 구조 정리를 위한 구버전 파일 삭제

--- lib/util/service/AuthService.dart
+++
@@ -1,9 +0,0 @@
-import 'package:mirim_oauth_flutter/mirim_oauth_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-MirimOAuth auth = MirimOAuth(
-  clientId: dotenv.env['CLIENT_ID']!,
-  clientSecret: dotenv.env['CLIENT_SECRET']!,
-  redirectUri: 'mirimpay://callback',
-  scopes: ['email', 'nickname'],
-);
